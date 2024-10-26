77cb903854b96087ed14ac65a4229a4bb602cbe9
Zahra Ebrahimzade <88291090+zhr-ebrahimzade@users.noreply.github.com>
2024-10-26 20:33:34 +0330

    adding b-cont

---
+++ LB/Controllers/BookController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagment.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
